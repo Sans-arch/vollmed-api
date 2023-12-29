421db0ec179d245e012c2132f6d87b4d04b805ff
Santiago Negreira <sansfleitas@gmail.com>
2023-12-29 00:28:21 -0300

    Create pagination

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,12 +3,14 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.entities.Medico;
 import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Especialidade;
 import med.voll.api.medico.MedicoDTO;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -26,8 +28,8 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar() {
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
 }
