a4b300d8f7ff64453970045ccf9044ed577e34aa
Santiago Negreira <sansfleitas@gmail.com>
2023-12-28 23:54:56 -0300

    Create bean validations using spring-boot-starter-validation

--- src/main/java/med/voll/api/medico/MedicoDTO.java
+++ src/main/java/med/voll/api/medico/MedicoDTO.java
@@ -1,4 +1,28 @@
 package med.voll.api.medico;
 
-public record MedicoDTO(String nome, String email, String crm, Especialidade especialidade, Endereco endereco) {
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record MedicoDTO(
+        @NotBlank
+        String nome,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+
+        @NotNull
+        Especialidade especialidade,
+
+        @NotNull
+        @Valid
+        Endereco endereco
+) {
 }
