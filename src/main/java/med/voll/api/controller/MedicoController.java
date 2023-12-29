bb599cba47e07e68307b4f2c4e3f5505be33b6e7
Santiago Negreira <sansfleitas@gmail.com>
2023-12-29 00:17:00 -0300

    Create List medicos method

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,14 +2,15 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.entities.Medico;
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.medico.Especialidade;
 import med.voll.api.medico.MedicoDTO;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,6 +23,11 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid MedicoDTO medicoDTO) {
         repository.save(new Medico(medicoDTO));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 
 }
---
+++ src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -0,0 +1,10 @@
+package med.voll.api.medico;
+
+import med.voll.api.entities.Medico;
+
+public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+
+    public DadosListagemMedico(Medico medico) {
+        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
